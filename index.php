ec2f823e1bcfa654c88c446c06063dcb464691de
金杉　有見子 <ykanasug@MBA-11AJ-047.local>
2016-04-07 13:38:59 +0900

    mysql was added.

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "Hello world"; ?>
+<?php
+
+echo "<h1>Helloooooo world</h1>";
+$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+if ($mysqli->connect_error) {
+    exit($mysqli->connect_error);
+} else {
+    $mysqli->set_charset("utf8");
+}
+$result = $mysqli->query("select 1") or exit($mysqli->error());
+echo var_dump($result->fetch_assoc());
+$mysqli->close();
+
+?>
